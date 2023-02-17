bf334186b190a18099e300d90d3d1d652000cb16
SwetlLOG <sweetlog@mail.ru>
2023-02-17 21:29:27 +0300

    README.md

---
+++ Example010_Array/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти элемент массива равный find и плюс генератор случ чисел в ме
+
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length; //возвращает длину или кол-во элю массива
+int find = 18;
+
+int index = 0;
+
+while ( index < n )
+
+{
+
+  if(array[index] == find)
+  {
+    Console.WriteLine(index);
+    break; //будет искать до первого элемента
+  }
+  index++;
+}
